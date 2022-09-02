101339bb8d6b6d4b4905031972fc7800229b0121
Renan <renan.fachin@hotmail.com>
2022-09-02 14:55:57 -0300

    Cria estrutura padrão que será seguida durante o projeto, define themes que serão seguidos durante o desenvolvimento e exclui arquivos desnecessários

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+export function Home() {
+  return (
+    <h1>Olá mundo!</h1>
+  )
+}
+
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+export default {
+    COLORS: {
+        BACKGROUND: "#000A0F",
+        BACKGROUND_200: "#001119",
+        BACKGROUND_300: "#0D1D25",
+        BRACKGROUND_HEADER: "#00111A",
+
+        GRAY: "#E1E1E6",
+        GRAY_100: "#C4C4C4",
+        GRAY_300: "#7C7C8A",
+
+        LOGO_COLOR: "#065E7C",
+        VALUE_TEXT: "#82F3FF",
+        BUTTON_SIGNIN: "#92000E",
+        BUTTON: "#750310",
+        WHITE: "#FFFFFF",
+        WHITE_200: "#FFFAF1",
+
+        TEXT_PENDING: "#FBA94C",
+        TEXT_OK: "#04D361",
+        TEXT_NOTOK: "#AB222E",
+        TEXT_DELETE: "#AB4D55",
+    }
+}
