b604939a17ac6d472cdc9bf8524f8613049fcc8e
Renan <renan.fachin@hotmail.com>
2022-09-07 20:16:36 -0300

    Alteração na rota da página details para receber um :id como query

--- src/Routes/app.routes.jsx
+++ src/Routes/app.routes.jsx
@@ -15,7 +15,7 @@ export function AppRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/details" element={<Details />} />
+            <Route path="/details/:id" element={<Details />} />
             <Route path="/orderhistory" element={<OrderHistory />} />
             <Route path="/orderpage" element={<OrderPage />} />
             {/* Colocar os outros */}
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,8 +5,24 @@ import { Button } from '../Button/index'
 import { BiMinus, BiPlus} from 'react-icons/bi'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 
+import { useState, useEffect } from 'react'
+
 export function Card({title, description, price, ...rest}){
     const imagem = `../../../src/assets/Pratos/${title}.png`
+
+    // Começando em 1 a quantidade
+    const [quantity, setQuantity] = useState(1)
+
+    function handleAddItem() {
+        setQuantity (quantity+1)
+    }
+
+    function handleRemoveItem() {
+        setQuantity (quantity-1)
+        if(quantity == 1){
+            setQuantity(1)
+        }
+    }
 
     return(
         <Container>
@@ -23,13 +39,19 @@ export function Card({title, description, price, ...rest}){
 
             <div className='AmountItemsAndBuy-wrapper'>
                 <div className='Amount'>
-                    <button className='MinusItem'>
+                    <button 
+                    className='MinusItem'
+                    onClick={handleRemoveItem}
+                    >
                         <BiMinus />
                     </button>
 
-                    <span>01</span>
+                    <span>0{quantity}</span>
                     
-                    <button className='PlusItem'>
+                    <button 
+                    className='PlusItem'
+                    onClick={handleAddItem}
+                    >
                         <BiPlus />
                     </button>
 
