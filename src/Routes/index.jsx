2a908200047238befb747c7bb63016bc2a4c68c3
Renan <renan.fachin@hotmail.com>
2022-09-29 17:04:59 -0300

    Adicionado o hook de autorização nas rotas para redirecionamento de login

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 
+// Importando o hook de autenticação
+import { useAuth } from '../hooks/authContext'
+
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes(){
+    // Pegando o dado do usuário de dentro do AuthContext.Provider
+    const { user } = useAuth();
+    
+    // Se o user existir, quer dizer que ele pode logar e será redirecionado para as rotas da aplicação
+    // Caso contrário, será mantido na rota de auth
+
     return(
         <BrowserRouter>
-            {/* <AppRoutes /> */}
-            <AuthRoutes />
+            {user ? <AppRoutes /> : <AuthRoutes />}
         </BrowserRouter>
     )
 }
