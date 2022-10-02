0b4969a4f907d593da14df1bdc664eb6b16461a5
Renan <renan.fachin@hotmail.com>
2022-10-02 00:48:46 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import GlobalStyles from './styles/global'
 import { AuthProvider } from './hooks/authContext'
 
 // Utilizando a ROUTES
-import { Routes } from './routes/index';
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
