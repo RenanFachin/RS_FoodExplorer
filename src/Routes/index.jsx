a3703bcb69f393e739974c66aa5339eb2d055197
Renan <renan.fachin@hotmail.com>
2022-10-02 00:39:59 -0300

    teste

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -7,11 +7,7 @@ import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes(){
-    // Pegando o dado do usuário de dentro do AuthContext.Provider
     const { user } = useAuth();
-    
-    // Se o user existir, quer dizer que ele pode logar e será redirecionado para as rotas da aplicação
-    // Caso contrário, será mantido na rota de auth
 
     return(
         <BrowserRouter>
